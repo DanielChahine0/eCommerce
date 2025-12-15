1f7458fea4e63f36333d9fe8d2ad5a16d4b2648d
DanielChahine0 <121309471+DanielChahine0@users.noreply.github.com>
2025-12-14 20:18:54 -0500

    Reformated backend

--- estore/src/main/java/com/example/estore/repository/TechRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.estore.repository;
-
-import com.example.estore.model.Tech;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TechRepository extends JpaRepository<Tech, Integer> {
-}
